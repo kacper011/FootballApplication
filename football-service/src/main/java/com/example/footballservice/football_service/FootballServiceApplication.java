a51b81ec69e2991541ab93d5cb0bee857d7c38e9
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-17 11:07:45 +0200

    Addition of football service

---
+++ football-service/src/main/java/com/example/footballservice/football_service/FootballServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballservice.football_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballServiceApplication.class, args);
+	}
+
+}
