22642cc721d87b91fc8fb839527543b5e465fcc6
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-20 11:41:12 +0200

    Addition of a league table

--- laliga-api/src/main/java/com/example/laliga_api/controller/LaLigaController.java
+++ laliga-api/src/main/java/com/example/laliga_api/controller/LaLigaController.java
@@ -1,18 +1,38 @@
 package com.example.laliga_api.controller;
 
 import com.example.laliga_api.model.Team;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.laliga_api.model.TeamPoints;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-//@RestController
-//@RequestMapping("/laliga")
-//public class LaLigaController {
-//
-//    @GetMapping("/teams")
-//    public List<Team> getAllTeams() {
-//        return null;
-//    }
-//}
+@RestController
+@RequestMapping("/laliga")
+public class LaLigaController {
+
+    private List<TeamPoints> leagueTable = new ArrayList<>();
+
+    @PostMapping("/addTeam")
+    public String addTeam(@RequestBody TeamPoints teamPoints) {
+        leagueTable.add(teamPoints);
+        return "Added team: " + teamPoints.getTeamName();
+    }
+
+    @PostMapping("/updatePoints")
+    public String updatePoints(@RequestParam String teamName, @RequestParam int points) {
+
+        for (TeamPoints team : leagueTable) {
+            if (team.getTeamName().equalsIgnoreCase(teamName)) {
+                team.setPoints(team.getPoints() + points);
+                return "Updated points for team: " + teamName + ", New points: " + team.getPoints();
+            }
+        }
+        return "Team not found" + teamName;
+    }
+
+    @GetMapping("/table")
+    public List<TeamPoints> getLeagueTable() {
+        return leagueTable;
+    }
+}
---
+++ laliga-api/src/main/java/com/example/laliga_api/model/TeamPoints.java
@@ -0,0 +1,15 @@
+package com.example.laliga_api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TeamPoints {
+
+    private String teamName;
+    private int points;
+
+}
