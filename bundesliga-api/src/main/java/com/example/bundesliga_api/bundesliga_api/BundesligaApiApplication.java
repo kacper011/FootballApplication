d1dc2d633e86c065cb06ffd54a1713789cf15952
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-22 17:59:57 +0200

    Addition of bundesliga_api to project

---
+++ bundesliga-api/src/main/java/com/example/bundesliga_api/bundesliga_api/BundesligaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bundesliga_api.bundesliga_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundesligaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BundesligaApiApplication.class, args);
+	}
+
+}
---
+++ bundesliga-api/src/test/java/com/example/bundesliga_api/bundesliga_api/BundesligaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bundesliga_api.bundesliga_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundesligaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
