5dafdb621ff6ec5f23042571a21a4592bcc91fd0
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-23 17:26:35 +0200

    Creation of DTOs

---
+++ bundesliga-api/src/main/java/com/example/bundesliga_api/bundesliga_api/dto/CoachDTO.java
@@ -0,0 +1,14 @@
+package com.example.bundesliga_api.bundesliga_api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CoachDTO {
+
+    private Long id;
+    private String name;
+    private String nationality;
+    private int age;
+}
