871df2cb9053a17b5a76e8b757696cf489acc7dc
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-26 17:09:34 +0200

    Addition of Premier League to application

---
+++ premierleague-api/src/main/java/com/example/premierleague_api/PremierleagueApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.premierleague_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierleagueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierleagueApiApplication.class, args);
+	}
+
+}
