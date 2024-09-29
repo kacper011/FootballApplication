cc3b6271a9326ac77c0a8af708671f6e0aee88c2
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-29 19:29:49 +0200

    Create a CoachMapper, PlayerMapper, TeamMapper in package mapper

---
+++ premierleague-api/src/main/java/com/example/premierleague_api/mapper/PlayerMapper.java
@@ -0,0 +1,48 @@
+package com.example.premierleague_api.mapper;
+
+import com.example.premierleague_api.dto.PlayerDTO;
+import com.example.premierleague_api.model.Player;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlayerMapper {
+
+    // Konwersja z encji Player na PlayerDTO
+    public static PlayerDTO toDTO(Player player) {
+        if (player == null) {
+            return null;
+        }
+
+        PlayerDTO playerDTO = new PlayerDTO();
+        playerDTO.setId(player.getId());
+        playerDTO.setName(player.getName());
+        playerDTO.setPosition(player.getPosition());
+        playerDTO.setNumber(player.getNumber());
+        playerDTO.setNationality(player.getNationality());
+        playerDTO.setAge(player.getAge());
+
+        // Przypisanie nazwy drużyny
+        if (player.getTeam() != null) {
+            playerDTO.setTeamName(player.getTeam().getName());
+        }
+        return playerDTO;
+    }
+
+    // Konwersja z PlayerDTO na encję Player
+    public Player toEntity(PlayerDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Player player = new Player();
+        player.setId(dto.getId());
+        player.setName(dto.getName());
+        player.setPosition(dto.getPosition());
+        player.setNumber(dto.getNumber());
+        player.setNationality(dto.getNationality());
+        player.setAge(dto.getAge());
+
+        return player;
+    }
+}
+
