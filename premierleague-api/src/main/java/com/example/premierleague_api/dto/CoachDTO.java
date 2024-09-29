45e05e855c269a08d8bb6bf46e711e697810f961
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-29 19:27:12 +0200

    Create a CoachDTO, PlayerDTO, TeamDTO in dto package

---
+++ premierleague-api/src/main/java/com/example/premierleague_api/dto/CoachDTO.java
@@ -0,0 +1,14 @@
+package com.example.premierleague_api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CoachDTO {
+
+    private Long id;
+    private String name;
+    private String nationality;
+    private int age;
+}
