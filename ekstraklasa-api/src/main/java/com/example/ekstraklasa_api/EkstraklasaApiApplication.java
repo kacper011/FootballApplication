36849fe9007332d3238029302b2d7f4332f9b7a2
Kacper Szabat <kacper.szabat99@gmail.com>
2024-10-03 11:20:14 +0200

    Addition ekstraklasa to the project

---
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/EkstraklasaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ekstraklasa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkstraklasaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkstraklasaApiApplication.class, args);
+	}
+
+}
---
+++ ekstraklasa-api/src/test/java/com/example/ekstraklasa_api/EkstraklasaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ekstraklasa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkstraklasaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
