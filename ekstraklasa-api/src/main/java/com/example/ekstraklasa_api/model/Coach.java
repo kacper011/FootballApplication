2e010e780aa52a17d5f6697150e7adc01038bc11
Kacper Szabat <kacper.szabat99@gmail.com>
2024-10-04 11:27:38 +0200

    Addition of model package to ekstraklasa-api

---
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/model/Coach.java
@@ -0,0 +1,28 @@
+package com.example.ekstraklasa_api.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "coaches")
+public class Coach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String nationality;
+    private int age;
+
+    @OneToOne(mappedBy = "coach")
+    @JsonBackReference
+    private Team team;
+}
+
---
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/model/Team.java
@@ -0,0 +1,44 @@
+package com.example.ekstraklasa_api.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String stadium;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "coach_id")
+    @JsonManagedReference
+    private Coach coach;
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Player> players;
+}
+
+/*
+JSON TO ADD TEAM
+{
+  "name" : "Real Madrid",
+  "stadium" : "Estadio Santiago Bernabéu",
+  "coach" : {
+    "name" : "Carlo Ancelotti",
+    "nationality" : "Italy",
+    "age" : 65
+  }
+}
+ */
