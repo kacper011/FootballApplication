c9d21671e4e0823efdc8d788fbb32c61be430c6b
Kacper Szabat <kacper.szabat99@gmail.com>
2024-10-06 19:19:31 +0200

    Addition of service package to ekstraklasa-api app

---
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/service/CoachService.java
@@ -0,0 +1,32 @@
+package com.example.ekstraklasa_api.service;
+
+import com.example.ekstraklasa_api.model.Coach;
+import com.example.ekstraklasa_api.repository.CoachRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CoachService {
+
+    @Autowired
+    private CoachRepository coachRepository;
+
+    public List<Coach> getAllCoaches() {
+        return coachRepository.findAll();
+    }
+
+    public Coach getCoachById(Long id) {
+        return coachRepository.findById(id)
+                .orElse(null);
+    }
+
+    public Coach saveCoach(Coach coach) {
+        return coachRepository.save(coach);
+    }
+
+    public void deleteCoach(Long id) {
+        coachRepository.deleteById(id);
+    }
+}
---
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/service/TeamService.java
@@ -0,0 +1,37 @@
+package com.example.ekstraklasa_api.service;
+
+import com.example.ekstraklasa_api.model.Team;
+import com.example.ekstraklasa_api.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeamService {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public List<Team> getAllTeams() {
+        return teamRepository.findAll();
+    }
+
+    public Team getTeamById(Long id) {
+        Optional<Team> team = teamRepository.findById(id);
+        return team.orElse(null);
+    }
+
+    public Team findByName(String name) {
+        return teamRepository.findByName(name);
+    }
+
+    public Team saveTeam(Team team) {
+        return teamRepository.save(team);
+    }
+
+    public void deleteTeam(Long id) {
+        teamRepository.deleteById(id);
+    }
+}
