068d1ae3d6729169d4f40b71cc0babefbf36fb86
Kacper Szabat <kacper.szabat99@gmail.com>
2024-10-08 17:38:45 +0200

    Addition to the project seriea_api

---
+++ seriea-api/src/test/java/com/example/seriea_api/SerieaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seriea_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerieaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
