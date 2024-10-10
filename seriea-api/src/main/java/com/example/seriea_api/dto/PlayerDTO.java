4f3c4216f4f51cfd99a63e92170000ca18d79bf0
Kacper Szabat <kacper.szabat99@gmail.com>
2024-10-10 16:56:44 +0200

    Addition of dto's package

---
+++ seriea-api/src/main/java/com/example/seriea_api/dto/PlayerDTO.java
@@ -0,0 +1,28 @@
+package com.example.seriea_api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlayerDTO {
+    private int id;
+    private String name;
+    private String position;
+    private int number;
+    private String nationality;
+    private int age;
+    private String teamName;
+}
+
+/*
+JSON TO ADD PLAYER
+{
+  "name" : "Daniel Carvajal",
+  "position" : "Defender",
+  "number" : 2,
+  "nationality" : "Spain",
+  "age" : 32,
+  "teamName" : "Real Madrid"
+}
+ */
---
+++ seriea-api/src/main/java/com/example/seriea_api/dto/TeamDTO.java
@@ -0,0 +1,29 @@
+package com.example.seriea_api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class TeamDTO {
+    private Long id;
+    private String name;
+    private String stadium;
+    private CoachDTO coach;
+    private List<PlayerDTO> players;
+}
+
+/*
+JSON TO ADD TEAM
+{
+  "name" : "Real Madrid",
+  "stadium" : "Estadio Santiago Bernabéu",
+  "coach" : {
+    "name" : "Carlo Ancelotti",
+    "nationality" : "Italy",
+    "age" : 65
+  }
+}
+ */
