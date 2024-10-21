1340859f2239ff2e7e2e0b661b5d55593a626995
Kacper Szabat <kacper.szabat99@gmail.com>
2024-10-21 12:41:52 +0200

    Addition of a transfer market application

---
+++ transfermarket/src/test/java/com/example/transfermarket/TransfermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transfermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
