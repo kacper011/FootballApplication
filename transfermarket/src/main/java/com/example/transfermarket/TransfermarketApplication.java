93e907210d403b2cf919aea29490b759b567e274
Kacper Szabat <kacper.szabat99@gmail.com>
2024-10-23 17:35:33 +0200

    Configuration modifications but still docker-compose does not work correctly

--- transfermarket/src/main/java/com/example/transfermarket/TransfermarketApplication.java
+++ transfermarket/src/main/java/com/example/transfermarket/TransfermarketApplication.java
@@ -2,8 +2,9 @@ package com.example.transfermarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {LoadBalancerAutoConfiguration.class})
 public class TransfermarketApplication {
 
 	public static void main(String[] args) {
