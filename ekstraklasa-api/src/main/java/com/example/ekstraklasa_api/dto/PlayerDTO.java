30da08dc325051c1058cf2fba958d9cef9138115
Kacper Szabat <kacper.szabat99@gmail.com>
2024-12-13 18:40:54 +0100

    Writing a test for Get All Players

--- ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/dto/PlayerDTO.java
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/dto/PlayerDTO.java
@@ -1,10 +1,12 @@
 package com.example.ekstraklasa_api.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PlayerDTO {
     private int id;
     private String name;
@@ -13,6 +15,25 @@ public class PlayerDTO {
     private String nationality;
     private int age;
     private String teamName;
+
+    public PlayerDTO(int id, String name, String position, int number, String nationality, int age) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.number = number;
+        this.nationality = nationality;
+        this.age = age;
+    }
+
+    public PlayerDTO(int id, String name, String position, int number, String nationality, int age, String teamName) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.number = number;
+        this.nationality = nationality;
+        this.age = age;
+        this.teamName = teamName;
+    }
 }
 
 /*
