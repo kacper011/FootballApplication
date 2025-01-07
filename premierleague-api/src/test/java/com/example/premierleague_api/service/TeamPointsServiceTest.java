d1450abd1fcaad8245dd0f6a7d73d68cfee3bf2d
Kacper Szabat <kacper.szabat99@gmail.com>
2025-01-07 21:16:29 +0100

    Writing a test for Add Team Success

---
+++ premierleague-api/src/test/java/com/example/premierleague_api/service/TeamPointsServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.premierleague_api.service;
+
+import com.example.premierleague_api.model.TeamPoints;
+import com.example.premierleague_api.repository.TeamPointsRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class TeamPointsServiceTest {
+
+    @Mock
+    private TeamPointsRepository teamPointsRepository;
+
+    @InjectMocks
+    private TeamPointsService teamPointsService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("Add Team Success")
+    @Test
+    public void testAddTeamSuccess() {
+
+        //Given
+        TeamPoints teamPoints = new TeamPoints(null, "New Team", 0, 0, 0, 0, 0);
+
+        when(teamPointsRepository.findAllByTeamName("New Team")).thenReturn(Collections.emptyList());
+        when(teamPointsRepository.save(teamPoints)).thenReturn(teamPoints);
+
+        //When
+        TeamPoints result = teamPointsService.addTeam(teamPoints);
+
+        //Then
+        assertNotNull(result);
+        assertEquals("New Team", result.getTeamName());
+
+        verify(teamPointsRepository, times(1)).findAllByTeamName("New Team");
+        verify(teamPointsRepository, times(1)).save(teamPoints);
+    }
+
+}
