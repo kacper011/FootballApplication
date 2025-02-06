f57d556f571ad0777086344428e8efe51f4e5eb5
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-06 18:20:36 +0100

    Add application config(name, version, owner)

--- ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/EkstraklasaApiApplication.java
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/EkstraklasaApiApplication.java
@@ -1,15 +1,25 @@
 package com.example.ekstraklasa_api;
 
+import com.example.ekstraklasa_api.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EkstraklasaApiApplication {
+public class EkstraklasaApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EkstraklasaApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
---
+++ ekstraklasa-api/src/main/java/com/example/ekstraklasa_api/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.example.ekstraklasa_api.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Value(("${app.owner}"))
+    private String appOwner;
+
+    public void printAppInfo() {
+        logger.info("Application Name: {}", appName);
+        logger.info("Application Version: {}", appVersion);
+        logger.info("Application Owner: {}", appOwner);
+    }
+}
