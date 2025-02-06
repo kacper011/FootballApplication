88a4d1aba7dce7359a7fc7062e41bb65d504d30b
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-06 17:54:02 +0100

    Add application config (name, version, owner)

--- bundesliga-api/src/main/java/com/example/bundesliga_api/BundesligaApiApplication.java
+++ bundesliga-api/src/main/java/com/example/bundesliga_api/BundesligaApiApplication.java
@@ -1,17 +1,27 @@
 package com.example.bundesliga_api;
 
+import com.example.bundesliga_api.config.AppConfig;
+import com.netflix.discovery.converters.Auto;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class BundesligaApiApplication {
+public class BundesligaApiApplication implements CommandLineRunner {
+	@Autowired
+	private AppConfig appConfig;
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(BundesligaApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
