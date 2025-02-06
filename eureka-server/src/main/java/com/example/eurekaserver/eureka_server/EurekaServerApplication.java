12fe285b2f1e4f8fb2649461c3d7a6fa2d0c99ad
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-06 18:24:08 +0100

    Add application config (name, version, owner)

--- eureka-server/src/main/java/com/example/eurekaserver/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/eureka_server/EurekaServerApplication.java
@@ -1,15 +1,25 @@
 package com.example.eurekaserver.eureka_server;
 
+import com.example.eurekaserver.eureka_server.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerApplication implements CommandLineRunner {
+
+	@Autowired
+	private AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
