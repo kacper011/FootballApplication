cf9212320474ccf1f2f7081b578250ec6a8f6365
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-06 17:48:38 +0100

    Add application config (name, version, owner)

--- api-gateway/src/main/java/com/example/apigateway/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/api_gateway/ApiGatewayApplication.java
@@ -1,15 +1,25 @@
 package com.example.apigateway.api_gateway;
 
+import com.example.apigateway.api_gateway.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiGatewayApplication {
+public class ApiGatewayApplication implements CommandLineRunner {
+
+	@Autowired
+	private AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
