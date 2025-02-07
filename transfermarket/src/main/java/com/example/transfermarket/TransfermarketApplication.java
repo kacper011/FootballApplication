166c983b743dfc4e8f719a0b597c7c0de156762e
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-07 18:39:44 +0100

    Add application config (name, version, owner)

--- transfermarket/src/main/java/com/example/transfermarket/TransfermarketApplication.java
+++ transfermarket/src/main/java/com/example/transfermarket/TransfermarketApplication.java
@@ -1,14 +1,24 @@
 package com.example.transfermarket;
 
+import com.example.transfermarket.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration;
 
 @SpringBootApplication(exclude = {LoadBalancerAutoConfiguration.class})
-public class TransfermarketApplication {
+public class TransfermarketApplication implements CommandLineRunner {
+
+	@Autowired
+	public AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransfermarketApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
