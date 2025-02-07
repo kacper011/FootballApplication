3ba24935530c1b20acc59b402abe9f4fa39d797b
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-07 18:24:48 +0100

    Add application config (name, version, owner)

--- laliga-api/src/main/java/com/example/laliga_api/LaLigaApiApplication.java
+++ laliga-api/src/main/java/com/example/laliga_api/LaLigaApiApplication.java
@@ -1,15 +1,25 @@
 package com.example.laliga_api;
 
+import com.example.laliga_api.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class LaLigaApiApplication {
+public class LaLigaApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaLigaApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
