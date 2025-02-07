02439855a15146643e499c4aee34e291a730685c
Kacper Szabat <kacper.szabat99@gmail.com>
2025-02-07 18:35:54 +0100

    Add application config (name, version, owner)

--- seriea-api/src/main/java/com/example/seriea_api/SerieaApiApplication.java
+++ seriea-api/src/main/java/com/example/seriea_api/SerieaApiApplication.java
@@ -1,13 +1,23 @@
 package com.example.seriea_api;
 
+import com.example.seriea_api.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SerieaApiApplication {
+public class SerieaApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SerieaApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appConfig.printAppInfo();
+	}
 }
